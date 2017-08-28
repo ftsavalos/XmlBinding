5a96acef9d0aa48a6d43b313f99c3f1829077971
Fotis Tsavalos <ft@eworx.gr>
2017-08-28 17:20:34 +0300

    small changes

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import com.example.demo.models.Data;
 import com.example.demo.models.VariableType;
 import com.example.demo.models.YearType;
+import com.sun.org.apache.xerces.internal.xni.parser.XMLDocumentFilter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,8 +11,17 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -32,16 +42,39 @@ public class DemoApplication implements CommandLineRunner {
     }
 
     @Override
-    public void run(String... strings) throws Exception {
-        File file = new File(xmlPath);
-        Data data = (Data) unmarshaller.unmarshal(file);
-        LOGGER.info("XML: {}", data);
-
+    public void run(String... strings) throws JAXBException {
+        Data data = this.parseWithJaxb();
         List<VariableType> variables = data.getVariables().getVariable();
         List<YearType> yearList = new ArrayList<>();
-
         printTime(() -> variables.forEach(a -> a.getYears().getYear().forEach(b -> yearList.add(b))));
         LOGGER.info("yearList size: {}", yearList.size());
+        printTime(() -> this.parseWithDocumentParser());
+    }
+
+    private void parseWithDocumentParser() {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = null;
+        Document doc = null;
+        try {
+            builder = factory.newDocumentBuilder();
+            doc = builder.parse(xmlPath);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        NodeList list = doc.getElementsByTagName("data");
+        LOGGER.info("{}", list.getLength());
+    }
+
+    private Data parseWithJaxb(){
+        File file = new File(xmlPath);
+        Data data = null;
+        try {
+            data = (Data) unmarshaller.unmarshal(file);
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
+        return data;
     }
 
     public static void main(String[] args) {
--- src/main/java/com/example/demo/util/Performance.java
+++ src/main/java/com/example/demo/util/Performance.java
@@ -2,20 +2,16 @@ package com.example.demo.util;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import java.time.Duration;
 import java.time.Instant;
-import java.util.concurrent.Callable;
-import java.util.function.Function;
 
 public class Performance {
 
     private final static Logger LOGGER = LoggerFactory.getLogger(Performance.class);
 
-    public static void printTime(Runnable runnable) throws Exception {
+    public static void printTime(Runnable runnable) {
         Instant now = Instant.now();
         runnable.run();
-        LOGGER.info("Operation took {} msecs", Duration.between(now, Instant.now()).toMillis());
+        LOGGER.info("Operation took {} milliseconds", Duration.between(now, Instant.now()).toMillis());
     }
-
 }
